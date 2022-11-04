317537b3772ca4309d4103193aeee87267bddb30
gingbae2287 <kingbae2287@gmail.com>
2022-11-04 15:37:00 +0900

    room system beginning

--- client/client.cpp
+++ client/client.cpp
@@ -42,25 +42,38 @@ void proc_recv(){
 }
 void Chat(){
     
-    char idx[]="1";
-    send(hSocket, idx, strlen(idx),0);
+    //char idx[]="1";
+    //send(hSocket, idx, strlen(idx),0);
     cout<<"<<Chat Mode>>\n\n";
     //==chat====
-    char cMsg[PACKET_SIZE]={};
+    char msg[PACKET_SIZE]={};
 
-    thread proc1(proc_recv);
-    while(1){
-        cout<<">> ";
-        cin.getline(cMsg,PACKET_SIZE);
+    thread chat(proc_recv);
+    while(!WSAGetLastError()){
+        /*
+        int tasknum=0;
+        cout<<"1 room list\n2 room create\n0 quit\n\n>> ";
+       cin>>tasknum;
+        if(tasknum==1) Chat();
+        else if(tasknum==2) ReversShell();
+        else if(tasknum==0) {
+            char idx[]="0";
+            send(hSocket, idx, strlen(idx),0);
+            cout<<"quit/n/n";
+            break;
+        }
+        else cout<<"worng number!=n";
+        */
+        cin.getline(msg,PACKET_SIZE,'\n');
+
         
-        send(hSocket, cMsg, strlen(cMsg),0); 
-        if(!strcmp(cMsg,"/q")) {
+        send(hSocket, msg, strlen(msg),0); 
+        if(!strcmp(msg,"/q")) {
             //cout<<"chat exit\n";
             break;
         }
-
     }
-    proc1.join();
+    chat.join();
     cout<<"chat exit\n";
     
 }
@@ -117,36 +130,105 @@ int main() {
     while(1) if(!connect(hSocket,(SOCKADDR*)&tAddr, sizeof(tAddr))) break;
     cout << "success connecting to server(IP >> " << inet_ntoa(tAddr.sin_addr) << ")\n" << endl;
     cout<<"name: ";
-    cin>>name;
+    cin.getline(name,NAME_LEGNTH,'\n');
     send(hSocket, name, strlen(name),0);
-    
-    thread chat(proc_recv);
-    while(!WSAGetLastError()){
-        /*
-        int tasknum=0;
-        cout<<"1 room list\n2 room create\n0 quit\n\n>> ";
-       cin>>tasknum;
-        if(tasknum==1) Chat();
-        else if(tasknum==2) ReversShell();
-        else if(tasknum==0) {
-            char idx[]="0";
-            send(hSocket, idx, strlen(idx),0);
+
+    char tasknum;
+    char roomName[100];
+    char buff[PACKET_SIZE];
+    int rResult=0;
+    int sResult=0;
+    while(1){
+        
+        ZeroMemory(&buff,PACKET_SIZE);
+
+        cout<<"\n1 room list\n2 room create\n3 enter room\n0 quit\n\n>> ";
+        cin.getline(buff,PACKET_SIZE,'\n');
+        sResult=send(hSocket, buff, PACKET_SIZE,0);
+        if(sResult==SOCKET_ERROR || sResult==0){
+                printf("error %s\n",WSAGetLastError());
+                break;
+        }
+        ZeroMemory(&buff,PACKET_SIZE);
+        rResult=recv(hSocket,buff,PACKET_SIZE,0);
+        if(rResult==SOCKET_ERROR || rResult==0){
+            printf("error %s\n",WSAGetLastError());
+            break;
+        }
+        printf("packet success %s\n",buff);
+        //cin.ignore();
+        tasknum=buff[0];
+        ZeroMemory(&buff,PACKET_SIZE);
+        
+        if(tasknum-48==1) {
+            printf("<<room list>>\n");
+            /*sResult=send(hSocket, &tasknum, PACKET_SIZE,0);
+            if(sResult==SOCKET_ERROR || sResult==0){
+                printf("error %s\n",WSAGetLastError());
+                break;
+            }*/
+ 
+            
+            rResult=recv(hSocket,buff,PACKET_SIZE,0);
+            if(rResult==SOCKET_ERROR || rResult==0){
+                printf("error %s\n",WSAGetLastError());
+                break;
+            }
+
+            printf("%s\n",buff);
+
+        }
+        else if(tasknum-48==2) {
+            ZeroMemory(&roomName,100);
+            printf("<<create room>>\nroom name: ");
+            cin>>roomName;
+            cin.ignore();
+            cin.clear();
+            sResult=send(hSocket, roomName, 100,0);
+            if(rResult==SOCKET_ERROR || rResult==0){
+                printf("error %s\n",WSAGetLastError());
+            break;
+        }
+        }
+        else if(tasknum-48==3) {
+            //enter room
+            ZeroMemory(&buff,PACKET_SIZE);
+            printf("<<enter room>>\nroom number: ");
+            cin>>buff;
+            cin.ignore();
+            cin.clear();
+
+            send(hSocket, buff, 2,0);
+            ZeroMemory(&buff,PACKET_SIZE);
+            rResult=recv(hSocket,buff,PACKET_SIZE,0);
+            if(rResult==SOCKET_ERROR || rResult==0){
+                printf("error %s\n",WSAGetLastError());
+                break;
+            }
+            if(!strcmp(buff,"0")){
+                printf("!enter room error\n\n");
+            }
+            else if(!strcmp(buff,"1")){
+                printf("erter room success. room num: %c\n\n",buff[0]);
+            }
+
+        }
+        else if(tasknum-48==0) {
+            //char idx[]="0";
+            //send(hSocket, idx, strlen(idx),0);
             cout<<"quit/n/n";
             break;
         }
-        else cout<<"worng number!=n";
-        */
-       char msg[PACKET_SIZE]={};
-        cout<<">> ";
-        cin.getline(msg,PACKET_SIZE);
+        else cout<<"worng number!\n";
         
-        send(hSocket, msg, strlen(msg),0); 
-        if(!strcmp(msg,"/q")) {
+        //cin.getline(msg,PACKET_SIZE);
+        
+       //send(hSocket, msg, strlen(msg),0); 
+        /*if(!strcmp(msg,"/q")) {
             //cout<<"chat exit\n";
             break;
-        }
+        }*/
     }
-    chat.join();
 
 /*
     
--- server/server.cpp
+++ server/server.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,6 +20,8 @@ using namespace std;
 #define NAME_LEGNTH 24
 #define ROOM_LEGNTH 100
 #define ROOM_PER_CLIENT 8
+#define ROOM_COUNT 8
+
 
 SOCKET clientSock[MAX_CLIENT];
 SOCKADDR_IN clientAddr[MAX_CLIENT];
@@ -27,27 +31,20 @@ SOCKET hListen;
 
 char buffer[PACKET_SIZE] = {};
 
-
-bool rooms[8];
-
-struct Room{
-    char roomName[ROOM_LEGNTH];
-    //int roomId;
-    bool isEmpty;
-    int clientCount;
-    Room(char RoomName[]){
-        strcpy(roomName,RoomName);
-        clientCount=0;
-        isEmpty=true;
-    }
-};
+struct Client;
+struct Room;
 
 struct Client{
     int sockId;
     bool isConnect;
     int roomId;
     char name[NAME_LEGNTH];
-    Client(){}
+    Client(){
+        roomId=-1;
+        isConnect=false;
+    }
+    bool operator==(const Client &cl1);
+    void Disconnect();
     /*Client(int SockId,char Name[]="default name"){
         sockId=SockId;
         strcpy(name,Name);
@@ -57,49 +54,59 @@ struct Client{
     ~Client(){}
 };
 
+struct Room{
+    char roomName[ROOM_LEGNTH];
+    //int roomId;
+    bool isEmpty;
+    int clientCount;
+    vector<Client> clients;
+    vector<Client>::iterator it;
+    Room(){
+        clientCount=0;
+        isEmpty=true;
+        clients.reserve(MAX_CLIENT);
+    }
+    void ClientEnter(Client &cl);
+    void ClientOut(Client &cl);
+    ~Room(){}
+};
+Room rooms[ROOM_COUNT];
 Client clients[MAX_CLIENT];
 
 
-/*
-void proc_recvs(){
-	
 
-	while(!WSAGetLastError()){
-		ZeroMemory(&buffer,PACKET_SIZE);
-		recv(Client, buffer, PACKET_SIZE,0);
-        if(!isChat) break;
-        if(!strcmp(buffer,"/q")) {
-            printf("other exited\n\n");
+void Chat(int clientIdx){
+    int rResult=0;
+    int sResult=0;
+    char tmp[PACKET_SIZE+NAME_LEGNTH+4];
+
+    char buff[PACKET_SIZE]={};
+    while(!WSAGetLastError()){
+        ZeroMemory(&buff,PACKET_SIZE);
+        rResult=recv(clientSock[clientIdx],buff,PACKET_SIZE,0);
+        if(rResult==SOCKET_ERROR || rResult==0){
+            //printf("error %d\n",packetSize);
             break;
         }
-		printf("recv: %s\n", buffer);
-	}
-}
-void Chat(){
-    isChat=true;
-    
-    cout<<"<<Chat mode>>\n\n";
-
-    thread proc2(proc_recvs);
-
-    char cMsg[PACKET_SIZE]={};
-    while(1){
-        printf(">>");
-        cin.getline(cMsg,PACKET_SIZE);
+        if(!strcmp(buff,"/q")) break;
+        strcpy(tmp,clients[clientIdx].name);
+        strcat(tmp,": ");
+        strcat(tmp,buff);
+        printf("%s\n",tmp);
         
-        send(hClient, cMsg, strlen(cMsg),0);
-        if(!strcmp(cMsg,"/q")) {
-            //cout<<"exit"<<endl;
-            break;
+        for(int i=0;i<MAX_CLIENT;i++){
+           
+            if(client[i]) {
+                
+                sResult= send(clientSock[i], tmp, strlen(tmp),0);
+                if(sResult== SOCKET_ERROR) printf("%d clnt send error %d\n",i, WSAGetLastError());
+            }
         }
         
     }
-    isChat=false;
-    proc2.join();
-    cout<<"exit"<<endl;
     
 }
-*/
+
 /*
 void ReversShell(){
     
@@ -135,10 +142,14 @@ void ClientPacket(int clientIdx){
     int rResult=0;
     int sResult=0;
     char clntName[NAME_LEGNTH];
-    char tmp[PACKET_SIZE+NAME_LEGNTH+4];
-    rResult=recv(clientSock[clientIdx],clntName,NAME_LEGNTH,0);
+    
+    char orderPacket;
+    char buff[PACKET_SIZE];
+    char roomName[ROOM_LEGNTH];
+
+    rResult=recv(clientSock[clientIdx],clntName,PACKET_SIZE,0);
     if(rResult==SOCKET_ERROR || rResult==0){
-            //printf("error %d\n",packetSize);
+            printf("error %d\n",rResult);
     }
     else{
         strcpy(clients[clientIdx].name,clntName);
@@ -146,33 +157,118 @@ void ClientPacket(int clientIdx){
         clients[clientIdx].sockId=clientIdx;
         printf("client %d's name: %s\n\n",clientIdx, clients[clientIdx].name);
     }
+
     while(!WSAGetLastError()){
-        char buff[PACKET_SIZE]={};
+        ZeroMemory(&orderPacket,sizeof(char));
+        ZeroMemory(&buff,PACKET_SIZE);
+        
         rResult=recv(clientSock[clientIdx],buff,PACKET_SIZE,0);
         if(rResult==SOCKET_ERROR || rResult==0){
-            //printf("error %d\n",packetSize);
+            printf("tasknum recv  error \n");
             break;
         }
-        if(!strcmp(buff,"/q")) break;
-        strcpy(tmp,clients[clientIdx].name);
-        strcat(tmp,": ");
-        strcat(tmp,buff);
-        printf("%s\n",tmp);
+        printf("packet success %s\n",buff);
+        orderPacket=buff[0];
+        ZeroMemory(&buff,PACKET_SIZE);
         
-        for(int i=0;i<MAX_CLIENT;i++){
-           
-            if(client[i]) {
-                
-                sResult= send(clientSock[i], tmp, strlen(tmp),0);
-                if(sResult== SOCKET_ERROR) printf("%d clnt send error %d\n",i, WSAGetLastError());
+        sResult=send(clientSock[clientIdx], &orderPacket, sizeof(char),0);
+        if(sResult==SOCKET_ERROR || sResult==0){
+                printf("error \n");
+                break;
+        }
+        int order=orderPacket-48;
+
+        if(order==1){
+            //room list
+            ZeroMemory(&buff,PACKET_SIZE);
+            //ZeroMemory(&roomName,ROOM_LEGNTH);
+            for(int i=0;i<ROOM_COUNT;i++){
+                if(!rooms[i].isEmpty) {
+                    /*sResult= send(clientSock[clientIdx], rooms[i].roomName, strlen(rooms[i].roomName),0);
+                    if(sResult==SOCKET_ERROR || sResult==0){
+                        printf("order send error %s\n",WSAGetLastError());
+                    }*/
+                    char idx=i+1+48;
+                    strncat(buff,&idx,1);
+                    strcat(buff,". ");
+                    strcat(buff,rooms[i].roomName);
+                    strcat(buff,"\n");
+                }
+            }
+            strcat(buff,"=======================\n\n");
+            sResult= send(clientSock[clientIdx], buff, PACKET_SIZE,0);
+            if(sResult==SOCKET_ERROR || sResult==0){
+                    printf("roomlist send error \n");
+                    break;
+            }
+            printf("<<<room list>>:\n%s",buff);
+
+        }
+        else if(order==2){
+            //create room
+            for(int i=0;i<ROOM_COUNT;i++){
+                if(rooms[i].isEmpty){
+                    ZeroMemory(&roomName,100);
+                    rResult=recv(clientSock[clientIdx],roomName,PACKET_SIZE,0);
+                    //printf("input: %s\n",roomName);
+                    if(rResult==SOCKET_ERROR || rResult==0){
+                        printf("error %d\n",rResult);
+                        break;
+                    }
+                    rooms[i].isEmpty=false;
+                    strcpy(rooms[i].roomName,roomName);
+                    printf(" create room. name: %s\n\n",rooms[i].roomName);
+                    //sResult= send(clientSock[i], tmp, strlen(tmp),0);
+                    break;
+                }
             }
         }
-        
+        else if(order==3){
+            //enter room
+            char roomNum;
+            rResult=recv(clientSock[clientIdx],&roomNum,2,0);
+            if(rResult==SOCKET_ERROR || rResult==0){
+                printf("error %d\n",rResult);
+                break;
+            }
+            int tmp=roomNum-48;
+            if(tmp>ROOM_COUNT || tmp<1){
+                printf("worng room number\n");
+                strcpy(buff,"0");
+                sResult= send(clientSock[clientIdx], buff, PACKET_SIZE,0);
+                if(sResult==SOCKET_ERROR || sResult==0){
+                    printf("enter room error\n");
+                    break;
+                }
+            }
+            else if(rooms[tmp-1].isEmpty){
+                printf("room %d was not created\n",tmp);
+                strcpy(buff,"0");
+                sResult= send(clientSock[clientIdx], buff, PACKET_SIZE,0);
+                if(sResult==SOCKET_ERROR || sResult==0){
+                    printf("enter room error\n");
+                    break;
+                }
+            }
+            else{
+                printf("client %d is enter room %d\n",clientIdx,tmp);
+                strcpy(buff,"1");
+                sResult= send(clientSock[clientIdx], buff, PACKET_SIZE,0);
+                if(sResult==SOCKET_ERROR || sResult==0){
+                    printf("enter room error\n");
+                    break;
+                }
+                clients[clientIdx].roomId=tmp-1;
+                rooms[tmp-1].clients.push_back(clients[clientIdx]);
+            }
+        }
     }
     client[clientIdx]=false;
+    clients[clientIdx].Disconnect();
     printf("client %d is disconnected\n",clientIdx);
     closesocket(clientSock[clientIdx]);
 }
+
 int main() {
     WSADATA wsaData;
     int iResult;
@@ -240,3 +336,30 @@ int main() {
     WSACleanup();
     return 0;
 }
+
+
+void Room::ClientOut(Client &cl){
+    it=find(clients.begin(),clients.end(),cl);
+    if(it!=clients.end()){
+        clients.erase(it);
+        return;
+    }
+}
+
+void Room::ClientEnter(Client &cl){
+        clients.push_back(cl);
+        return;
+}
+bool Client::operator==(const Client &cl1){
+    return (name==cl1.name);
+}
+void Client::Disconnect(){
+        isConnect=false;
+        if(roomId!=-1){
+            rooms[roomId].ClientOut(*this);
+            roomId=-1;
+        }
+        ZeroMemory(&name,NAME_LEGNTH);
+
+       return;
+    }
